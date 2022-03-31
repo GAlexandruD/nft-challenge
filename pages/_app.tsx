f8779aeab01985319da143bdefc1c83d6ebec2e9
AlexandruD <AlexandruD@hotmail.com>
2022-03-31 16:27:17 +0300

    day 2 challenge coding along

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+
+//Rinkeby test network
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/search.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const Search = (props: Props) => {
+  return <div>search</div>
+}
+
+export default Search
