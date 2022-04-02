e471ca4c120118c860843fbea733082ad27b9305
AlexandruD <AlexandruD@hotmail.com>
2022-04-02 11:32:27 +0300

    adding filtering to cards. work in progress...

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,48 @@ import ParticlesTwo from '../components/Particles'
 import { sanityClient, urlFor } from '../sanity'
 import { Collection } from '../typing'
 
+import { Fragment, useEffect, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+
 interface Props {
   collections: Collection[]
 }
 
 const Home = ({ collections }: Props) => {
-  console.log(collections.length)
-  // collections.length >1 ? 'className includes grid' : className includes flex
+  const [localCollections, setLocalCollections] = useState(collections)
+  console.log({ localCollections })
+
+  const results = [
+    { name: '1 Card' },
+    { name: '2 Cards' },
+    { name: '3 Cards' },
+    { name: 'All Cards' },
+  ]
+  const [selected, setSelected] = useState(results[3])
+
+  const filtering: any = {
+    cardOne: localCollections.slice(0, 1),
+    cardTwo: localCollections.slice(0, 2),
+    cardThree: localCollections.slice(0, 3),
+    carDAll: localCollections,
+  }
+  const expertFunction = (props: string) => {
+    console.log({ props })
+    console.log('bau')
+    console.log(filtering.props)
+  }
+
+  const [filtered, setFiltered] = useState(localCollections)
+
+  const [showingCollections, setShowingCollections] = useState(localCollections)
+  console.log({ showingCollections })
+
+  useEffect(() => {
+    expertFunction(selected.name)
+    console.log({ selected })
+  }, [selected])
+
   return (
     <div className="">
       <Head>
@@ -41,8 +76,8 @@ const Home = ({ collections }: Props) => {
             {
               //For multiple collections change flex to grid
             }
-            {collections.map((collection) => (
-              <Link href={`/nft/${collection.slug.current}`}>
+            {collections.map((collection, idx) => (
+              <Link key={idx} href={`/nft/${collection.slug.current}`}>
                 <div className="m-2 flex cursor-pointer flex-col items-center rounded-xl bg-gradient-to-b from-teal-800/40 to-teal-800/90 shadow-2xl transition-all duration-200 hover:scale-105">
                   <div className="mt-4 rounded-xl p-2">
                     <img
@@ -63,6 +98,64 @@ const Home = ({ collections }: Props) => {
             ))}
           </div>
         </main>
+        <h1 className="self-center pt-4 text-yellow-200">Showing</h1>
+        <div id="listbox" className="mb-32 w-32 self-center">
+          <Listbox value={selected} onChange={setSelected}>
+            <div className="relative mt-1">
+              <Listbox.Button className=" relative w-full cursor-default rounded-lg bg-teal-800/90 py-2 pl-3 pr-10 text-left text-yellow-200 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <span className="block truncate">{selected.name}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <SelectorIcon
+                    className="h-5 w-5 text-yellow-200"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-teal-800 py-1 text-base text-yellow-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {results.map((person, personIdx) => (
+                    <Listbox.Option
+                      key={personIdx}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active
+                            ? 'bg-amber-100 text-teal-900'
+                            : 'bg-teal-700 text-yellow-200'
+                        }`
+                      }
+                      value={person}
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? 'font-medium' : 'font-normal'
+                            }`}
+                          >
+                            {person.name}
+                          </span>
+                          {selected ? (
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox>
+        </div>
       </div>
     </div>
   )
@@ -97,7 +190,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }`
 
   const collections = await sanityClient.fetch(query)
-  console.log(collections)
 
   return {
     props: {
