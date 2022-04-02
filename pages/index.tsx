6af8f8002362015bd2d29657a8802d2e47197b3a
AlexandruD <AlexandruD@hotmail.com>
2022-04-02 09:11:18 +0300

    some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,14 +23,14 @@ const Home = ({ collections }: Props) => {
       <ParticlesTwo />
 
       <div className=" absolute left-0 right-0 mx-auto flex min-h-screen max-w-7xl flex-col  py-20 px-10 2xl:px-0">
-        <h1 className="mb-10 text-4xl font-extralight">
+        <h1 className="mb-10 text-4xl font-extralight text-yellow-200">
           The{' '}
           <span className="font-extrabold underline decoration-pink-600/50">
             PAPAFAM
           </span>{' '}
           NFT MARKET Place
         </h1>
-        <main className="rounded-xl bg-yellow-200/50 p-10 shadow-xl shadow-rose-400/20">
+        <main className="rounded-xl bg-yellow-200/50 p-10 shadow-xl shadow-yellow-300/20">
           <div
             className={`${
               collections.length > 1
@@ -43,15 +43,18 @@ const Home = ({ collections }: Props) => {
             }
             {collections.map((collection) => (
               <Link href={`/nft/${collection.slug.current}`}>
-                <div className="flex cursor-pointer flex-col items-center transition-all duration-200 hover:scale-105">
-                  <img
-                    className=" h-96 w-60 rounded-2xl object-cover"
-                    src={urlFor(collection.mainImage).url()}
-                    alt=""
-                  />
-                  <div className="p-5">
+                <div className="m-2 flex cursor-pointer flex-col items-center rounded-xl bg-gradient-to-b from-teal-800/40 to-teal-800/90 shadow-2xl transition-all duration-200 hover:scale-105">
+                  <div className="mt-4 rounded-xl p-2">
+                    <img
+                      className="h-96 w-60 rounded-xl object-cover shadow-xl"
+                      src={urlFor(collection.mainImage).url()}
+                      alt="Card image"
+                    />
+                  </div>
+                  <div className="p-5 text-yellow-200">
                     <h2>{collection.title}</h2>
-                    <p className="mt-2 text-sm text-gray-200">
+                    <hr className="" />
+                    <p className="mt-2 text-sm text-yellow-100">
                       {collection.description}
                     </p>
                   </div>
